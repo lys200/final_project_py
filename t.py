2b04d8b81f6e5c4baa5fbecadfadd230832f0c0b
nherlyse morisset <nherlysemorisset@gmail.com>
2024-07-13 20:06:21 -0400

    almost done

--- t.py
+++
@@ -1,5 +0,0 @@
-p = "MOI"
-print(' '*20, f"Voici les informations des salles"
-                                "se trouvant au {p} batiment .\n")
-print(' '*20, "Voici les informations des salles"
-                               f"se trouvant au batiment {p}.\n")
